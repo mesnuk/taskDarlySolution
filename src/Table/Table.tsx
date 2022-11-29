4b67efc887781eb886b612a4d68242538e5ace41
unknown <samsuser570@gmail.com>
2022-11-29 16:25:09 +0200

    table head

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,1 +1,20 @@
 import React from "react";
+
+interface HeadingsProps{
+     headers : string[]
+}
+
+const TableHead = (props : HeadingsProps) => {
+     return <tr>
+          {props.headers.map((el, i) => <th key={el + i}>{el}</th>)}
+     </tr>
+}
+
+export const Table : React.FC = () => {
+     const headers: string[] = ["1", "2"] ;
+
+
+     return <table>
+          <TableHead headers={headers} />
+     </table>
+}
