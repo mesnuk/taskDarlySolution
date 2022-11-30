7f69aac7956de369843f9c71dfc87f18cb9822f1
Andrii Sovtus <99980555+mesnuk@users.noreply.github.com>
2022-11-30 21:18:57 +0200

    loading

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,15 +1,13 @@
-import React, {FormEvent, useRef, useState} from 'react'
+import React, {FormEvent, useState} from 'react'
 import './Form.scss'
-import {Logger, Value} from "sass";
-import {log} from "util";
 import axios from "axios";
-import {throws} from "assert";
+import set = Reflect.set;
+
 
 
 
 export const Form : React.FC<{setUsers : Function}> = ({setUsers}) => {
-    const [loading, setLoading] = useState(false);
-
+    const [loading, setLoading]: [boolean, Function] = useState<boolean>(false);
 
     const handleSubmit : Function = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -20,16 +18,15 @@ export const Form : React.FC<{setUsers : Function}> = ({setUsers}) => {
             country : '',
             city : ''
         }
-
+        setLoading(true)
         const inputs = Array.from((event.target as HTMLFormElement).elements)
             .filter((el) => el.tagName === 'INPUT')
             .map(el => (el as HTMLInputElement));
-
+        console.log(loading)
         inputs.forEach(el => {
             data[el.name as keyof Fields] = el.value;
-            el.disabled = loading;
-            console.log(loading)
-            // el.value = '';
+            el.disabled = !loading;
+            el.value = '';
         })
 
         axios.post<Fields>('http://localhost:3001/users', {...data, id : `${new Date()}`})
@@ -42,31 +39,31 @@ export const Form : React.FC<{setUsers : Function}> = ({setUsers}) => {
     return (
 
     <div className='main-form__layout'>
-        {loading && <h1>...</h1>}
         <h1>Form</h1>
         <form className='main-form' onSubmit={(e) => handleSubmit(e)} >
             <label>
                 Firstname:
-                <input type="text" name='firstname' minLength={3} required/>
+                <input type="text" name='firstname' minLength={3} required disabled={loading}/>
             </label>
             <label>
                 Lastname:
-                <input type="text" name='lastname' minLength={3} required/>
+                <input type="text" name='lastname' minLength={3} required disabled={loading}/>
             </label>
             <label>
                 Email:
-                <input type="email" name='email'/>
+                <input type="email" name='email' disabled={loading}/>
             </label>
             <label>
                 Country:
-                <input type="text" name='country'  minLength={3} required/>
+                <input type="text" name='country'  minLength={3} required disabled={loading}/>
             </label>
             <label>
                 City:
-                <input type="city" name='city' minLength={3} required/>
+                <input type="city" name='city' minLength={3} required disabled={loading} />
             </label>
-            <button className='main-form__button' type='submit' >Send</button>
+            <button className='main-form__button' type='submit'disabled={loading}>Send</button>
         </form>
+        {loading && <h1>...</h1>}
     </div>
     )
 }
